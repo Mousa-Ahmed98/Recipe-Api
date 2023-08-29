5dad3a8c9bb265d9e226fd641d3830e1f1bc0c74
Mousa-98 <fricces05@gmail.com>
2023-08-30 00:07:34 +0300

    Update recipe entity

--- Core/Entities/Recipe.cs
+++ Core/Entities/Recipe.cs
@@ -11,6 +11,7 @@ namespace Core.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Image { get; set; }
+        public string? Category { get; set; }
 
         public ICollection<Ingredient> ingredients { get; set; }
         public ICollection<Step> steps { get; set; }
