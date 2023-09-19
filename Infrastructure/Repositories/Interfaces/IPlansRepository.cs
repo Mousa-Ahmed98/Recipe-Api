f6e8f45503aad953859c9f7e6015aae985b64316
mohammed0xff <mohammed0xff@gmail.com>
2023-09-20 01:34:43 +0300

    add `PlansRepository`

---
+++ Infrastructure/Repositories/Interfaces/IPlansRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.CustomModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Interfaces
+{
+    public interface IPlansRepository : IBaseRepository<Recipe>
+    {
+        Task<List<Plan>> GetAllPlans();
+        Task<bool> PlanOff(int planId);
+        Task<Plan> PlanOut(string day, int recipeId);
+        Task<bool> ChangePlanDate(int planId, string day2);
+    }
+}
