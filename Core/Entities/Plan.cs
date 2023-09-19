9c3b33a8c385c7f4a18df9fe8c5792cfde710f48
mohammed0xff <mohammed0xff@gmail.com>
2023-09-20 01:32:03 +0300

    add `Plan` entity to db

---
+++ Core/Entities/Plan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Core.Entities
+{
+    public class Plan
+    {
+        public int Id { get; set; }
+        public DateTime Day { get; set; }
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
