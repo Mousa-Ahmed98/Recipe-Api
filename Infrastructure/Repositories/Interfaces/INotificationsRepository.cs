9c9dd18de6373c31d9f2588b95f719394f11b7b5
mohammed0xff <mohammed0xff@gmail.com>
2023-09-25 15:51:22 +0300

    add notifications repository

---
+++ Infrastructure/Repositories/Interfaces/INotificationsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Core.Enum;
+using Core.Interfaces;
+using Infrastructure.CustomModels;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Interfaces
+{
+    public interface INotificationsRepository : IBaseRepository<Notification>
+    {
+        Task<PaginatedList<Notification>> GetRecentNotifications(string userId, int pageNumber, int pageSize);
+        Task ReadRecentNotifications(string userId);
+        Task AddNewNotification(string userId, int recipeId, NotificationType type);
+    }
+}
