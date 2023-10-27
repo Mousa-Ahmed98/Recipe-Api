1aa474a84e6023fa8b4ee47787af070ccf257f68
mohammed0xff <mohammed0xff@gmail.com>
2023-10-27 11:02:50 +0200

    refact : add seperate configurations for better readablity and clarity in program.cs file.

---
+++ Recipe/Configurations/QuartzConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Application.Services;
+
+namespace RecipeApi.Configurations
+{
+    public static class QuartzConfiguration
+    {
+        public static void ConfigureQuartz(this IServiceCollection services) {
+            
+            // Configure Quartz.NET scheduler
+            services.AddQuartz(q =>
+            {
+                // Add the job and specify the job type
+                q.AddJob<PlansReminder>(j =>
+                    j.WithIdentity(nameof(PlansReminder), "group1")
+                    );
+
+                // Configure the trigger for the job
+                q.AddTrigger(t => t
+                    .ForJob(nameof(PlansReminder), "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(s => s
+                        .WithInterval(TimeSpan.FromDays(1)) // everyday 
+                        .RepeatForever()));
+            });
+
+            // Configure the Quartz.NET hosted service
+            services.AddQuartzHostedService(options =>
+            {
+                options.WaitForJobsToComplete = true;
+                options.AwaitApplicationStarted = true;
+            });
+
+        }
+    }
+}
