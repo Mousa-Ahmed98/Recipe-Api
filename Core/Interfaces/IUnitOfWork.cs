4362b68ac4addfb450baf3c28675b13dd4842ace
mohammed0xff <mohammed0xff@gmail.com>
2023-10-27 10:56:56 +0200

    feat : add unit of work for managing repositories

---
+++ Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Core.Interfaces.Repositories;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        ICategoriesRepository CategoriesRepository { get; }
+        IRecipeRepository RecipeRepository { get; }
+        IIngredientsRepository IngredientsRepository { get; }
+        IStepsRepository StepsRepository { get; }
+        IUsersRepository UsersRepository { get; }
+        INotificationsRepository NotificationsRepository { get; }
+        IPlansRepository PlansRepository { get; }
+        IFollowsRepository FollowsRepoisitory { get; }
+
+        Task<bool> SaveAsync();
+    }
+}
