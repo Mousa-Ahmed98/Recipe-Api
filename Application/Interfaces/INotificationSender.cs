9650c22a21ebf411bdd2f9c0e72f1529d978d75d
mohammed0xff <mohammed0xff@gmail.com>
2023-10-27 17:32:08 +0200

    add `NotificationSender` for handling realtime notification sending using our HubContext & getting connectionIds from `ConnectedUserManager`

---
+++ Application/Interfaces/INotificationSender.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.Response;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Interfaces
+{
+    public interface INotificationSender
+    {
+        Task NotifyOne(string userId, NotificationResponse notification);
+        Task NotifyMany(List<string> userIds, NotificationResponse notification);
+    }
+}
